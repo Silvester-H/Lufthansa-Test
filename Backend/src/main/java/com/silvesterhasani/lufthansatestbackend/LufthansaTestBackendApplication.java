fd19cab03d83052e194eb7ab61834dceff70358b
Silvester Hasani <36986878+Silvester-H@users.noreply.github.com>
2022-01-30 15:54:13 +0100

    Created Project

---
+++ Backend/src/main/java/com/silvesterhasani/lufthansatestbackend/LufthansaTestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.silvesterhasani.lufthansatestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LufthansaTestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LufthansaTestBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/silvesterhasani/lufthansatestbackend/LufthansaTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvesterhasani.lufthansatestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LufthansaTestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
